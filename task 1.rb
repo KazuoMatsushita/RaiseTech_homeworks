21aa6f55d86139166893c29bec858a840d6f3a09
KazuoMatsushita <65701896+KazuoMatsushita@users.noreply.github.com>
2020-05-22 10:22:52 +0900

    Add files via upload

---
+++ task 1.rb
@@ -0,0 +1,13 @@
+puts "Ruby 「How many times do you come here？」"
+value = gets.to_i
+case value
+when 1
+    puts "Ruby「Hello world!」"
+when 2
+    puts "Ruby「So we meet again!」"
+when 3
+    puts "Ruby「We are already friends!」"
+end
+
+puts "Ruby「Goodbye」"
+puts "Ruby「See you again!」"
