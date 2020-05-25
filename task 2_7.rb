ce46e6a7371d43870b2012257edf24a72bebb29d
KazuoMatsushita <65701896+KazuoMatsushita@users.noreply.github.com>
2020-05-25 18:15:10 +0900

    第2回の課題−7
    番号の追加

--- task 2_7.rb
+++ task 2_7.rb
@@ -1,6 +1,6 @@
 class Communication
     attr_accessor
-    #職場かどうかを判断するメソッドを作って、職場じゃなければ挨拶しないようにしてみよう
+    #職場かどうかを判断するメソッドを作って、職場でなければ挨拶しないようにしてみよう
     def greed
         puts "where are you?"
         @call = gets.to_s
